924a548afcd4bf98663d28b8611d6b9665495912
Kristófer <kristoferrt@gmail.com>
2016-10-21 20:22:02 +0000

    Add Board and Player Entities

---
+++ src/main/is/TicTacToe/data/Board.java
@@ -0,0 +1,13 @@
+package is.TicTacToe;
+
+public class Board {
+    private final int SIZE = 3;
+    private String grid[SIZE][SIZE];
+
+    public Board() { }
+
+    public String getSymbol(int x, int y) { return ""; }
+    public void setSymbol(int x, int y, String symbol) {  }
+
+    public void init() {  }
+}
---
+++ src/main/is/TicTacToe/data/Player.java
@@ -0,0 +1,12 @@
+package is.TicTacToe;
+
+public class Player {
+    private String name;
+    private String symbol;
+
+    public String getName() { return ""; }
+    public String getSymbol() { return ""; }
+
+    public void setName(String n) {  }
+    public void setSymbol(Strin s) {  }
+}
