a8c990afb315238c755aac2fff1acd32d4509d24
Kristófer <kristoferrt@gmail.com>
2016-10-21 20:37:06 +0000

    Add TicTacToeService

---
+++ src/main/is/TicTacToe/service/TicTacToeService.java
@@ -0,0 +1,12 @@
+package is.TicTacToe;
+
+public class TicTacToeService {
+    private Game game;
+
+    public TicTacToeService() {  }
+
+    public void makeMove() {  }
+    public void startGame() {  }
+
+    public String getGameResult() { return ""; }
+}
