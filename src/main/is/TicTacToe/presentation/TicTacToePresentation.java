5c2891e6b85eb30d4d663bfd7452a8a3c8ba66aa
Kristófer <kristoferrt@gmail.com>
2016-10-21 20:39:55 +0000

    Add TicTacToePresentation

---
+++ src/main/is/TicTacToe/presentation/TicTacToePresentation.java
@@ -0,0 +1,13 @@
+package is.TicTacToe;
+
+import is.TicTacToe.TicTacToeService;
+
+public class TicTacToePresentation {
+    private TicTacToeService gameService;
+
+    public TicTacToePresentation() {  }
+
+    public static int main(String args[]){
+        return 0;
+    }
+}
