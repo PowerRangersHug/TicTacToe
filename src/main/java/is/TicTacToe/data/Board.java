2412f5181e6873491dfd0d0b775c360ecb6470f4
Fanney <fanneykri13@ru.is>
2016-10-23 19:16:44 +0000

    fixed according to review

--- src/main/java/is/TicTacToe/data/Board.java
+++ src/main/java/is/TicTacToe/data/Board.java
@@ -5,104 +5,110 @@ public class Board {
     private String[][] grid;
 
     /**
-	* The constructor for the board
- 	* @param 
- 	* @return 
- 	*/
+    * The constructor for the board
+    * @param 
+    * @return 
+    */
     public Board() 
     {
-    	init();
+        init();
     }
 
     /**
-	* Get the current symbol at the (x,y) grid position.
- 	* @param int x, int y
- 	* @return a symbol
- 	*/
+    * Get the current symbol at the (x,y) grid position.
+    * @param int x, int y
+    * @return a symbol
+    */
     public String getSymbol(int x, int y) 
     {
-    	if ((x < 0 || x > 2) && (y < 0 || y > 2))
-    		throw new IndexOutOfBoundsException("Index out of bounds!");
-
-    	return grid[x][y];
+        if ((x < 0 || x > 2) || (y < 0 || y > 2))
+            throw new IndexOutOfBoundsException("Index out of bounds!");
+        return grid[x][y];
     }
 
     /**
-	* Set the current symbol at the (x,y) grid position.
- 	* @param int x, int y
- 	* @return
- 	*/
+    * Set the current symbol at the (x,y) grid position.
+    * @param int x, int y
+    * @return
+    */
     public void setSymbol(int x, int y, String symbol)
     {
-    	if ((x < 0 || x > 2) && (y < 0 || y > 2))
-    		throw new IndexOutOfBoundsException("Index out of bounds!");
+        if ((x < 0 || x > 2) || (y < 0 || y > 2))
+            throw new IndexOutOfBoundsException("Index out of bounds!");
 
-    	if (symbol == "X" || symbol == "O")
-    		grid[x][y] = symbol;
+        if (symbol == "X" || symbol == "O")
+            grid[x][y] = symbol;
     }
 
     /**
-	* Initialize the board, starting fresh (new game).
- 	* @param
- 	* @return
- 	*/
+    * Initialize the board, starting fresh (new game).
+    * @param
+    * @return
+    */
     public void init() 
     {
-    	// Points to a new 3x3 grid
-    	grid = new String[SIZE][SIZE];
-    	for (int i = 0; i < SIZE; i++)
-    	{
-    		for (int j = 0; j < SIZE; j++)
-    		{
-    			grid[i][j] = "";
-    		}
-    	}
+        // Points to a new 3x3 grid
+        grid = new String[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                grid[i][j] = "";
+            }
+        }
     }
 
     /**
-	* Checks if the board is in a winning state, i.e.
-	* a symbol occurs three times in a row on the board.
- 	* @param
- 	* @return true/false whether the board has three in row or not.
- 	*/
+    * Checks if the board is in a winning state, i.e.
+    * a symbol occurs three times in a row on the board.
+    * @param
+    * @return true/false whether the board has three in row or not.
+    */
     public boolean hasThreeInRow()
     {
-    	if (diagonalsThreeInRow())
-    		return true;
-    	for (int i = 0; i < SIZE; i++)
-    	{
-    		if (hasThreeInCurrentRow(i))
-    			return true;
-    	}	
-    	return false; 
+        if (diagonalsThreeInRow())
+        	return true;
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (hasThreeInCurrentRow(i))
+                return true;
+            else if (hasThreeInCurrentColumn(i))
+                return true;
+        }
+        return false; 
     }
 
     /**
-	* Checks if the board is full of symbols, i.e. the game
-	* is over.
- 	* @param
- 	* @return true/false whether the board is full or not.
- 	*/
+    * Checks if the board is full of symbols, i.e. the game
+    * is over.
+    * @param
+    * @return true/false whether the board is full or not.
+    */
     public boolean isFull() 
     {
-    	for (int i = 0; i < 3; i++)
-    	{
-    		for (int j = 0; j < 3; j++)
-    		{
-    			if (grid[i][j] == "")
-    				return false;
-    		}
-    	}
-    	return true;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (grid[i][j] == "")
+                    return false;
+            }
+        }
+        return true;
     }
 
     private boolean diagonalsThreeInRow()
     {
-    	return (grid[0][0] != "") && (grid[0][0] == grid[1][1]) && (grid[1][1] == grid[2][2]);
+        return (grid[0][0] != "") && (grid[0][0] == grid[1][1]) && (grid[1][1] == grid[2][2]);
     }
 
     private boolean hasThreeInCurrentRow(int i)
     {
-    	return (grid[i][0] != "") && (grid[i][0] == grid[i][1]) && (grid[i][1] == grid[i][2]);
+        return (grid[i][0] != "") && (grid[i][0] == grid[i][1]) && (grid[i][1] == grid[i][2]);
+    }
+
+    private boolean hasThreeInCurrentColumn(int i)
+    {
+        return (grid[0][i] != "") && (grid[0][i] == grid[1][i]) && (grid[1][i] == grid[2][i]);
     }
 }
--- src/test/java/is/TicTacToe/data/BoardTest.java
+++ src/test/java/is/TicTacToe/data/BoardTest.java
@@ -7,65 +7,65 @@ import org.junit.rules.ExpectedException;
 import org.junit.Rule;
 
 public class BoardTest {
-	@Rule public final ExpectedException thrown = ExpectedException.none();
+    @Rule public final ExpectedException thrown = ExpectedException.none();
 
 
-	@Test
+    @Test
     public void testBoardSetAndGet()
     {
-    	Board testBoard = new Board();
-    	testBoard.setSymbol(1,1,"X");
-		assertEquals("X", testBoard.getSymbol(1,1));
-	}
+        Board testBoard = new Board();
+        testBoard.setSymbol(1,1,"X");
+       assertEquals("X", testBoard.getSymbol(1,1));
+    }
 
-	@Test
+    @Test
     public void testBoardSetAndGetWrongSymbol()
     {
-    	Board testBoard = new Board();
-    	testBoard.setSymbol(1,1,"BLA");
-		assertEquals("", testBoard.getSymbol(1,1));
-	}
+        Board testBoard = new Board();
+        testBoard.setSymbol(1,1,"BLA");
+       assertEquals("", testBoard.getSymbol(1,1));
+    }
 
-	@Test
+    @Test
     public void testBoardSetWrongField()
     {
-    	Board testBoard = new Board();
-    	thrown.expect(IndexOutOfBoundsException.class);
-		thrown.expectMessage("Index out of bounds!");
-    	testBoard.setSymbol(3,3,"X");
-	}
+        Board testBoard = new Board();
+        thrown.expect(IndexOutOfBoundsException.class);
+        thrown.expectMessage("Index out of bounds!");
+        testBoard.setSymbol(3,3,"X");
+    }
 
-	@Test
+    @Test
     public void testBoardGetWrongField()
     {
-    	Board testBoard = new Board();
-    	thrown.expect(IndexOutOfBoundsException.class);
-		thrown.expectMessage("Index out of bounds!");
-    	testBoard.getSymbol(3,3);
-	}
+        Board testBoard = new Board();
+        thrown.expect(IndexOutOfBoundsException.class);
+        thrown.expectMessage("Index out of bounds!");
+        testBoard.getSymbol(3,3);
+    }
 
-	@Test
+    @Test
     public void testBoardThreeInRow()
     {
-    	Board testBoard = new Board();
-    	testBoard.setSymbol(0,0,"X");
-    	testBoard.setSymbol(1,1,"X");
-    	testBoard.setSymbol(2,2,"X");
-		assertEquals(true, testBoard.hasThreeInRow());
-	}
+        Board testBoard = new Board();
+        testBoard.setSymbol(0,0,"X");
+        testBoard.setSymbol(1,1,"X");
+        testBoard.setSymbol(2,2,"X");
+        assertEquals(true, testBoard.hasThreeInRow());
+    }
 
 
-	@Test
+    @Test
     public void testBoardIsFull()
     {
-    	Board testBoard = new Board();
-    	for (int i = 0; i < 3; i++)
-    	{
-    		for (int j = 0; j < 3; j++)
-    		{
-    			testBoard.setSymbol(i,j,"O");
-    		}
-    	}
-		assertEquals(true, testBoard.isFull());
-	}
+        Board testBoard = new Board();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                testBoard.setSymbol(i,j,"O");
+            }
+        }
+        assertEquals(true, testBoard.isFull());
+    }
 }
