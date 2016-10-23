ab3549b2cc34a10056bc6fe24d2f7a6daffba323
Þórey Jóna Guðjónsdóttir <thoreyg13@ru.is>
2016-10-23 18:33:33 +0000

    Add BoardNotNull test

--- src/main/java/is/TicTacToe/data/Game.java
+++ src/main/java/is/TicTacToe/data/Game.java
@@ -7,7 +7,14 @@ public class Game {
     private Board board;
     private Player p1, p2;
 
-    public Board getBoard() { return new Board(); }
+    public Game() 
+    {
+    	board = new Board();
+    	p1 = new Player();
+    	p2 = new Player();
+    }
+
+    public Board getBoard() { return board; }
     public Player getPlayer(String name) { return new Player(); }
 
     public boolean isDone() { return false; }
--- src/test/java/is/TicTacToe/data/GameTest.java
+++ src/test/java/is/TicTacToe/data/GameTest.java
@@ -1,7 +1,18 @@
-package is.TicTacToe;
+package is.TicTacToe.data;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
+import is.TicTacToe.data.Game;
+import is.TicTacToe.data.Board;
+
 public class GameTest {
+
+	@Test
+	public void TestBoardNotNull() 
+	{
+		Game newGame = new Game();
+		assertNotNull(newGame.getBoard());
+	}	
 }
