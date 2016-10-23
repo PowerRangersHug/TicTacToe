f8436c93d8659588c7fb37582d66c533dc78e1e2
Fanney <fanneykri13@ru.is>
2016-10-23 18:18:35 +0000

    Board initialization and get/set functions

--- src/main/java/is/TicTacToe/data/Board.java
+++ src/main/java/is/TicTacToe/data/Board.java
@@ -2,14 +2,28 @@ package is.TicTacToe.data;
 
 public class Board {
     private final int SIZE = 3;
-    private String[][] grid = new String[SIZE][SIZE];
+    private String[][] grid;
 
-    public Board() { }
+    public Board() 
+    {
+    	grid = new String[SIZE][SIZE];
+    }
 
-    public String getSymbol(int x, int y) { return ""; }
-    public void setSymbol(int x, int y, String symbol) {  }
+    public String getSymbol(int x, int y) 
+    {
+    	return grid[x][y];
+    }
 
-    public void init() {  }
+    public void setSymbol(int x, int y, String symbol)
+    {
+    	grid[x][y] = symbol;
+    }
+
+    public void init() 
+    {
+    	// Points to a new 3x3 grid
+    	grid = new String[SIZE][SIZE];
+    }
 
     public boolean hasThreeInRow() { return false; }
     public boolean isFull() { return false; }
--- src/test/java/is/TicTacToe/data/BoardTest.java
+++ src/test/java/is/TicTacToe/data/BoardTest.java
@@ -3,5 +3,14 @@ package is.TicTacToe;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import is.TicTacToe.data.Board;
+
 public class BoardTest {
+	@Test
+    public void testBoardSetAndGet()
+    {
+    	Board testBoard = new Board();
+    	testBoard.setSymbol(1,1,"X");
+		assertEquals("X", testBoard.getSymbol(1,1));
+	}
 }
