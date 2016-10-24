6fbb853dc6baea3772d7947f12bab0bd53958a2b
herdismaria <herdismaria90@gmail.com>
2016-10-24 11:30:52 +0200

    Started the presentation layer, Welcome messageprints

--- src/main/is/TicTacToe/presentation/TicTacToePresentation.java
+++ src/main/is/TicTacToe/presentation/TicTacToePresentation.java
@@ -2,12 +2,28 @@ package is.TicTacToe;
 
 import is.TicTacToe.service.TicTacToeService;
 
-public class TicTacToePresentation {
+public class TicTacToePresentation 
+{
     private TicTacToeService gameService;
 
-    public TicTacToePresentation() {  }
+    public TicTacToePresentation() 
+    {
+    	gameService = new gameService();
+    }
 
-    public static int main(String args[]){
+    public void WelcomeMessage()
+    {
+    	System.out.println("Welcome to TicTacToe!\n");
+    }
+
+    /*public void GetGameMode()
+    {
+    	System.out.println()
+    }*/
+
+    public static int main(String args[])
+    {
+    	WelcomeMessage();
         return 0;
     }
 }
--- src/test/is/TicTacToe/service/TicTacToeServiceTest.java
+++ src/test/is/TicTacToe/service/TicTacToeServiceTest.java
@@ -3,5 +3,16 @@ package is.TicTacToe;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class TicTacToeServiceTest {
+public class TicTacToePresentationTest 
+{
+	@Test
+	public void TestWelcomeMessage()
+	{
+		TicTacToePresentation pres = new TicTacToePresentation();
+
+		assertEquals("Welcome to TicTacToe", pres.WelcomeMessage());
+	}
+
+	//@Test
+	//public void 
 }
