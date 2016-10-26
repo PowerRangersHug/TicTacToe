cd45fae92f0699fa5a74f3ea2ae5ad15dbf5b84e
Þórey Jóna Guðjónsdóttir <thoreyg13@ru.is>
2016-10-26 19:46:48 +0000

    Changed constructor to take in parameters (decrease dependency)

--- src/main/java/is/TicTacToe/data/Game.java
+++ src/main/java/is/TicTacToe/data/Game.java
@@ -7,15 +7,41 @@ public class Game {
     private Board board;
     private Player p1, p2;
 
-    public Game() 
+    /**
+    * Initialize new instance of Game class with params
+    * @param b Board
+    * @param p1, Player 1
+    * @param p2, Player 2
+    */
+    public Game(Board b, Player p1, Player p2)
     {
-        board = new Board();
-        p1 = new Player("Player1", "X");
-        p2 = new Player("Player2", "O");
+        this.board = b;
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
     public Board GetBoard() { return board; }
-    public Player GetPlayer(String name) { return new Player(); }
+    
+    /**
+    * Get player with name 'name'
+    * @param name, Player name
+    * @return Player
+    */
+    public Player GetPlayer(String name) 
+    {
+        if (p1.GetName() == name)
+        {
+            return p1;
+        }
+        else if (p2.GetName() == name)
+        {
+            return p2;
+        }
+        else 
+        {
+            return null;
+        }
+    }
 
     public boolean IsDone() { return false; }
     public Player GetWinner() { return new Player(); }
--- src/test/java/is/TicTacToe/data/GameTest.java
+++ src/test/java/is/TicTacToe/data/GameTest.java
@@ -9,27 +9,31 @@ import is.TicTacToe.data.Board;
 
 public class GameTest {
 
-   
-
     @Test
     public void TestBoardNotNull() 
     {
-        Game testGame = new Game();
+        Game testGame = new Game(new Board(), new Player("Player1", "X"), new Player("Player2", "O"));
         assertNotNull(testGame.GetBoard());
     }
 
     @Test
     public void TestPlayerOneNotNull()
     {
-        Game testGame = new Game();
+        Game testGame = new Game(new Board(), new Player("Player1", "X"), new Player("Player2", "O"));
         assertNotNull(testGame.GetPlayer("Player1"));
     }
 
     @Test
     public void TestPlayerTwoNotNull()
     {
-        Game testGame = new Game();
+        Game testGame = new Game(new Board(), new Player("Player1", "X"), new Player("Player2", "O"));
         assertNotNull(testGame.GetPlayer("Player2"));
     }
 
+    @Test
+    public void TestPlayerOneName() 
+    {
+        Game testGame = new Game(new Board(), new Player("Player1", "X"), new Player("Player2", "O"));
+        assertEquals("Player1", testGame.GetPlayer("Player1").GetName());
+    }
 }
