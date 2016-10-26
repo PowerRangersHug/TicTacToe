3ba6ff268788ac120c82fc8315ea47a283541570
Sara Kristjánsdóttir <sarakristjansdottir@clients-pool-1.nat.ru.is>
2016-10-26 21:52:52 +0000

    Constructors for TicTacToeService

--- src/main/java/is/TicTacToe/service/TicTacToeService.java
+++ src/main/java/is/TicTacToe/service/TicTacToeService.java
@@ -5,7 +5,21 @@ import is.TicTacToe.data.Game;
 public class TicTacToeService {
     private Game game;
 
-    public TicTacToeService() {  }
+    public TicTacToeService() 
+    {
+    	Player p1 = new Player("Player1" , "X");
+        Player p2 = new Player("Player2" , "O");
+        Board b = new Board();
+        game = new Game(b , p1 , p2);
+    }
+
+    public TicTacToeService(String player1, String player2) 
+    {
+        Player p1 = new Player(player1, "X");
+        Player p2 = new Player(player2 , "O");
+        Board b = new Board();
+        game = new Game(b , p1 , p2);
+    }
 
     public void makeMove() {  }
     public void startGame() {  }
