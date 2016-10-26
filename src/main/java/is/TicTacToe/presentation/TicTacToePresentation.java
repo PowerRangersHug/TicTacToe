5ad4f541aa616fe847858de0c5e2718114a6411b
Fanney <fanneykri13@ru.is>
2016-10-26 19:05:08 +0000

    Input checking on startup

--- src/main/java/is/TicTacToe/presentation/TicTacToePresentation.java
+++ src/main/java/is/TicTacToe/presentation/TicTacToePresentation.java
@@ -2,30 +2,62 @@ package is.TicTacToe.presentation;
 import java.util.Scanner;
 import is.TicTacToe.service.TicTacToeService;
 
+
 public class TicTacToePresentation 
 {
     private TicTacToeService gameService;
 
     public TicTacToePresentation() 
     {
-    	gameService = new TicTacToeService();
+        gameService = new TicTacToeService();
     }
 
     public static void WelcomeMessage()
     {
-    	System.out.println("Welcome to TicTacToe!");
+        System.out.println("Welcome to TicTacToe!");
     }
 
-    public static void GetGameMode()
+    public static int GetGameMode()
     {
-    	System.out.println("Please choose the game mode");
-        System.out.println("1 - 1 Player");
-        System.out.println("2 - 2 Player");
+        Scanner sc = new Scanner(System.in);
+        String inputLine = "";
+        while (true)
+        {
+            System.out.println("Please choose the game mode");
+            System.out.println("1 - 1 Player");
+            System.out.println("2 - 2 Player");
+            System.out.println("Q - Quit");
+            if(sc.hasNextLine())
+            {
+                inputLine = sc.next();
+            }
+
+            if (inputLine.equals("Q"))
+            {
+                return 0;
+            }
+            else if (inputLine.equals("1"))
+            {
+                return 1;
+            }
+            else if (inputLine.equals("2"))
+            {
+                return 2;
+            }
+            else
+            {
+                System.out.println("Wrong input!");
+            }
+        }
     }
 
     public static void main(String args[])
     {
-    	WelcomeMessage();
-        GetGameMode();
+        WelcomeMessage();
+        int gameMode = GetGameMode();
+        if (gameMode == 0)
+        {
+                System.out.println("Goodbye");
+        }
     }
 }
