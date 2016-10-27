93d796d2d3ab0a4cd9f9967f32e3ccf10464ace9
Kristófer <kristoferrt@gmail.com>
2016-10-27 18:06:17 +0000

    Add Player functionality in TicTacToeService.
    Unit tests passed.
    
    Also added prototype functions for presentation layer.

--- src/main/java/is/TicTacToe/service/TicTacToeService.java
+++ src/main/java/is/TicTacToe/service/TicTacToeService.java
@@ -1,28 +1,98 @@
 package is.TicTacToe.service;
 
 import is.TicTacToe.data.Game;
+import is.TicTacToe.data.Player;
+import is.TicTacToe.data.Board;
 
 public class TicTacToeService {
     private Game game;
+    private String playerOne;
+    private String playerTwo;
 
-    public TicTacToeService() 
+    /**
+     * Initialize the service class with default values for Player instances.
+     */
+    public TicTacToeService()
     {
-    	Player p1 = new Player("Player1" , "X");
-        Player p2 = new Player("Player2" , "O");
+        playerOne = "Player1";
+        playerTwo = "Player2";
+        Player p1 = new Player(playerOne , "X");
+        Player p2 = new Player(playerTwo , "O");
         Board b = new Board();
-        game = new Game(b , p1 , p2);
+        game = new Game(b, p1, p2);
     }
 
-    public TicTacToeService(String player1, String player2) 
+    /**
+     * Initialize the service class with customized names for Player instances.
+     * @param player1 Player 1 name
+     * @param player2 Player 2 name
+     */
+    public TicTacToeService(String player1, String player2)
     {
-        Player p1 = new Player(player1, "X");
-        Player p2 = new Player(player2 , "O");
+        playerOne = player1;
+        playerTwo = player2;
+        Player p1 = new Player(playerOne, "X");
+        Player p2 = new Player(playerTwo, "O");
         Board b = new Board();
-        game = new Game(b , p1 , p2);
+        game = new Game(b, p1, p2);
     }
 
-    public void makeMove() {  }
-    public void startGame() {  }
+    public void MakeMove() {  }
 
-    public String getGameResult() { return ""; }
+    /**
+     * Start a game in the specified mode, with the specified player names.
+     * @param mode Which mode will be played; Human vs. Human,
+     * Human vs. Computer or Computer vs. Computer
+     * @param p1Name Player 1 name
+     * @param p2Name Player 2 name
+     */
+    public void StartGame(int mode, String p1Name, String p2Name)
+    {
+        SetPlayerNames(p1Name, p2Name);
+    }
+
+    /**
+     * Get the game result.
+     * @return Game result in String format
+     */
+    public String GetGameResult() { return ""; }
+
+    /**
+     * Change the current games' Player names.
+     * @param p1 Player 1 name
+     * @param p2 Player 2 name
+     */
+    public void SetPlayerNames(String p1, String p2)
+    {
+        if(!p1.isEmpty())
+        {
+            GetPlayerByName(playerOne).SetName(p1);
+            playerOne = p1;
+        }
+        if(!p2.isEmpty())
+        {
+            GetPlayerByName(playerTwo).SetName(p2);
+            playerTwo = p2;
+        }
+    }
+
+    /**
+     * Find a Player currently playing with the name stored in pName.
+     * @param pName Name of the player to find
+     * @return Player instance that has the specififed name, if the player is
+     * found. If a Player isn't found null is returned.
+     */
+    public Player GetPlayerByName(String pName)
+    {
+        return game.GetPlayer(pName);
+    }
+
+    /**
+     * Get a String representation of the current game board.
+     * @return String representation of the game board
+     */
+    public String GetBoard()
+    {
+        return "";
+    }
 }
--- src/test/java/is/TicTacToe/service/TicTacToeServiceTest.java
+++ src/test/java/is/TicTacToe/service/TicTacToeServiceTest.java
@@ -1,9 +1,42 @@
 package is.TicTacToe;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
-public class TicTacToeServiceTest 
-{
+import is.TicTacToe.service.TicTacToeService;
 
+public class TicTacToeServiceTest
+{
+    @Test
+    public void TestGetPlayerByName()
+    {
+        // Test GetPlayerByName() with default constructor
+        TicTacToeService testService = new TicTacToeService();
+
+        assertNull(testService.GetPlayerByName("PlayerThatDoesntExist"));
+        assertEquals("Player1", testService.GetPlayerByName("Player1").GetName());
+        assertEquals("Player2", testService.GetPlayerByName("Player2").GetName());
+    }
+
+    @Test
+    public void TestConstructorWithParameters()
+    {
+        // Test constructor with parameters
+        TicTacToeService testService = new TicTacToeService("John", "Eve");
+
+        assertEquals("John", testService.GetPlayerByName("John").GetName());
+        assertEquals("Eve", testService.GetPlayerByName("Eve").GetName());
+    }
+
+    @Test
+    public void TestSetPlayerNames()
+    {
+        TicTacToeService testService = new TicTacToeService();
+        testService.SetPlayerNames("John", "Eve");
+
+        assertEquals("John", testService.GetPlayerByName("John").GetName());
+        assertEquals("Eve", testService.GetPlayerByName("Eve").GetName());
+    }
 }
