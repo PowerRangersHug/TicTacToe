9d6b0f168d5ef2beef29d86235d6202aa0efd484
Sara Kristjánsdóttir <sarakristjansdottir@clients-pool-1.nat.ru.is>
2016-10-28 19:28:30 +0000

    test for making a random move for the computer when the board is full

--- src/test/java/is/TicTacToe/service/TicTacToeServiceTest.java
+++ src/test/java/is/TicTacToe/service/TicTacToeServiceTest.java
@@ -119,6 +119,22 @@ public class TicTacToeServiceTest
         assertEquals(false, testService.GetGame().GetBoard().IsEmpty());
     }
 
+    @Test
+    public void TestMakeMoveForComputerWhenBoardIsFull()
+    {
+        TicTacToeService testService = new TicTacToeService("Player1");
+        testService.MakeMove(0, 0, "Player1");
+        testService.MakeMove(0, 1, "Player1");
+        testService.MakeMove(0, 2, "Player1");
+        testService.MakeMove(1, 0, "Player1");
+        testService.MakeMove(1, 1, "Player1");
+        testService.MakeMove(1, 2, "Player1");
+        testService.MakeMove(2, 0, "Player1");
+        testService.MakeMove(2, 1, "Player1");
+        testService.MakeMove(2, 2, "Player1");
+        assertEquals(false, testService.MakeMove());
+    }
+
 
 }
 
