dd0d16d1c905be895e966a79a3be83a97331b87e
Sara Kristjánsdóttir <sarakristjansdottir@Saras-MacBook-Air.local>
2016-10-29 19:14:09 +0000

    Testing BoardThreeInRow when there is a column with three in row

--- src/test/java/is/TicTacToe/data/BoardTest.java
+++ src/test/java/is/TicTacToe/data/BoardTest.java
@@ -19,12 +19,22 @@ public class BoardTest {
     }
 
     @Test
-    public void TestBoardThreeInRow()
+    public void TestBoardThreeInRowDiagonal()
     {
         Board testBoard = new Board();
         testBoard.SetSymbol(0,0,"X");
         testBoard.SetSymbol(1,1,"X");
         testBoard.SetSymbol(2,2,"X");
+        assertEquals(true, testBoard.HasThreeInRow());
+    }
+
+    @Test
+    public void TestBoardThreeInRowColumns()
+    {
+        Board testBoard = new Board();
+        testBoard.SetSymbol(0,0,"X");
+        testBoard.SetSymbol(0,1,"X");
+        testBoard.SetSymbol(0,2,"X");
         assertEquals(true, testBoard.HasThreeInRow());
     }
 
