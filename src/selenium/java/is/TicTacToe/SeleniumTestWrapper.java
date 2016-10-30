2893389c9e3e7c629b1d0221db3df3ebc67022be
Kristófer R <kristoferrt@gmail.com>
2016-10-30 17:03:46 +0000

    Try selenium with chromium

--- src/selenium/java/is/TicTacToe/SeleniumTestWrapper.java
+++ src/selenium/java/is/TicTacToe/SeleniumTestWrapper.java
@@ -3,58 +3,26 @@ package is.TicTacToe;
 import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public abstract class SeleniumTestWrapper {
-    //static ChromeDriver driver;
-    static FirefoxDriver driver;
+    static ChromeDriver driver;
     static String baseUrl;
     static String port;
 
     @BeforeClass
-    public static void openBrowser() throws IOException
+    public static void openBrowser()
     {
-        //driver = new ChromeDriver();
-        String travisCiFlag = System.getenv().get("TRAVIS");
-        FirefoxBinary firefoxBinary  = "true".equals(travisCiFlag) ?
-            getFirefoxBinaryForTravisCi() : new FirefoxBinary();
-
-        driver = new FirefoxDriver(firefoxBinary, new FirefoxProfile());
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        //port = System.getenv("PORT");
-				port = "8080";
+        port = System.getenv("PORT");
         if (port == null) {
             port = "8080";
         }
         //baseUrl = "http://localhost:" + port;
         baseUrl = "https://powerrangers-tictactoe-staging.herokuapp.com/";
-    }
-
-    private static FirefoxBinary getFirefoxBinaryForTravisCi() throws IOException
-    {
-        String firefoxPath = getFirefoxPath();
-        System.out.println("firefox path: " + firefoxPath);
-
-        return new FirefoxBinary(new File(firefoxPath));
-    }
-
-    private static String getFirefoxPath() throws IOException {
-        ProcessBuilder pb = new ProcessBuilder("which", "firefox");
-        pb.redirectErrorStream(true);
-        Process process = pb.start();
-        try (InputStreamReader isr = new InputStreamReader(process.getInputStream(), "UTF-8");
-             BufferedReader br = new BufferedReader(isr))
-            {
-                return br.readLine();
-            }
     }
 
     @AfterClass
